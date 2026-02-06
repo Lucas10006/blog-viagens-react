e81f60d6167c009c4e46f5ea3a131fc5c46f069d
Rushi10006 <160591260+Lucas10006@users.noreply.github.com>
2026-02-06 23:30:39 +0100

    Initial commit - React blog setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Blog de Viagens</h1>
+      <p>Descobre histórias, destinos e aventuras pelo mundo 🌍</p>
+    </div>
+  )
+}
+
+export default Home
